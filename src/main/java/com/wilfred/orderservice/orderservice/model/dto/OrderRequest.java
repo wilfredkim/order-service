0c741e36553cfdea6e5fc66175e70280879723c8
Wilfred <wilfredkim5@gmail.com>
2024-05-10 20:23:55 +0300

    update on service k8s file

--- src/main/java/com/wilfred/orderservice/orderservice/model/dto/OrderRequest.java
+++ src/main/java/com/wilfred/orderservice/orderservice/model/dto/OrderRequest.java
@@ -1,9 +1,10 @@
 package com.wilfred.orderservice.orderservice.model.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, @NotEmpty String orderNumber, @NotEmpty String skuCode, @NotEmpty BigDecimal price,
+public record OrderRequest(Long id, @NotEmpty String orderNumber, @NotEmpty String skuCode, @NotNull BigDecimal price,
                            Integer quantity) {
 }
