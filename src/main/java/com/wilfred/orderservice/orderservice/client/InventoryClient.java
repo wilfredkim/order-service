04baed6d1fd6038f929cd840ec68d1ad58538c33
Wilfred <wilfredkim5@gmail.com>
2024-05-12 13:51:56 +0300

    refactor openfiegn to rest clients and add secrets and config map yaml for k8s

--- src/main/java/com/wilfred/orderservice/orderservice/client/InventoryClient.java
+++ src/main/java/com/wilfred/orderservice/orderservice/client/InventoryClient.java
@@ -1,14 +1,13 @@
 package com.wilfred.orderservice.orderservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/inventories")
+    @GetExchange(value = "/api/v1/inventories")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 
--- src/main/java/com/wilfred/orderservice/orderservice/service/OrderService.java
+++ src/main/java/com/wilfred/orderservice/orderservice/service/OrderService.java
@@ -5,6 +5,7 @@ import com.wilfred.orderservice.orderservice.model.Order;
 import com.wilfred.orderservice.orderservice.model.dto.OrderRequest;
 import com.wilfred.orderservice.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
 
     public Order placeOrder(OrderRequest orderRequest) {
+        log.info("SkuCode::::::::::::::::"+orderRequest.skuCode());
+        log.info("Quantity::::::::::::::::"+orderRequest.quantity());
+
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if (isProductInStock) {
             Order order = Order.builder()
